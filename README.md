c1f6c8ffdb92e7506ffe91ea41f2bd5eded95275
CJNatas <cjchargreaves@gmail.com>
2021-06-11 15:28:34 -0700

    commit

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Started by planning out how the program would look in plan.text
 I then created the computerPick function to allow a randomized choice for the computer.
 
 Based on that, I still need to add the rules for the choices selected depending on the button.
-
--- site.js
+++ site.js
@@ -1,57 +1,33 @@
-<<<<<<< HEAD
-=======
+let userInput = prompt('What do you choose? rock, paper, or scissors?');
+let computerInput;
 
->>>>>>> 39760fec6f8f3857a567b78d9fb641d769763939
-const userInput = prompt('What do you choose? rock, paper, or scissors?');
-const playerSelection = userInput;
-const computerSelection = computerPick();
-
-
-
-
-<<<<<<< HEAD
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-
-
-
-function playerPick(){
-
-}
-
-function computerPick(){
-    let things = ['Rock', 'Paper', 'Scissors'];
+function computerPlay(){
+    let things = ['rock', 'paper', 'sissors'];
     let thing = things[Math.floor(Math.random()*things.length)];
-    console.log('The computer chose: ' + thing);
+    alert('Computer Chose: ' + thing);
 }
 
-=======
-
-function computerPick(){
-    let things = ['rock', 'paper', 'scissors'];
-    let computerInput = things[Math.floor(Math.random()*things.length)];
-    return computerInput;
-}
+const playerScore = 0;
+const computerScore = 0;
+const playerSelection = userInput;
+const computerSelection = computerPlay();
 
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection == rock && computerSelection == scissors){
-        alert('Player wins!');
-    }   else if (playerSelection == rock && computerSelection == paper){
-        alert('Computer wins!');
-    }   else (playerSelection == rock && computerSelection == rock)
-        alert('It is a draw!');
-    }
+    
+function playRound(playerSelection, computerSelection) {
+        playerSelection = playerSelection.toLowerCase();
+        if (playerSelection === computerSelection){
+            alert('its a tie!');
+        }   else if (playerSelection === "rock" && computerSelection === "paper"){
+            alert('you lose!');
+        }   else if (playerSelection === "paper" && computerSelection === "scissors"){
+            alert('you lose!');
+        }   else if (playerSelection === "scissors" && computerSelection === "rock"){
+            alert('you lose!');
+        }   else {
+            alert('you win!');
+        }
+    
+}  
 
->>>>>>> 39760fec6f8f3857a567b78d9fb641d769763939
-
-function playRound(playerSelection, computerSelection){
-    if (playerSelection == rock && computerSelection == scissors){
-        alert('Player wins!');
-    }   else if (playerSelection == rock && computerSelection == paper){
-        alert('Computer wins!');
-    }   else if (playerSelection == rock && computerSelection == rock){
-        alert('It is a draw!');
-    }  
-}
+playRound(playerSelection, computerSelection);
