d8af96366138db8e896490768a9100d02f23ac9e
CJNatas <cjchargreaves@gmail.com>
2021-06-12 01:45:49 -0700

    commit

--- site.js
+++ site.js
@@ -1,33 +1,62 @@
-let userInput = prompt('What do you choose? rock, paper, or scissors?');
-let computerInput;
+
+function userInput(){
+    let choiceMap = '';
+    let makeChoice = prompt('what do you choose?');
+    if (makeChoice === 'rock'){
+        return makeChoice;
+    } else if (makeChoice === 'paper'){
+        return makeChoice;
+    } else if (makeChoice ==='scissors'){
+        return makeChoice;
+    } else
+        alert('invalid restart game');
+    }  
 
 function computerPlay(){
-    let things = ['rock', 'paper', 'sissors'];
+    let things = ['rock', 'paper', 'scissors'];
     let thing = things[Math.floor(Math.random()*things.length)];
     alert('Computer Chose: ' + thing);
+    return thing;
 }
 
-const playerScore = 0;
-const computerScore = 0;
-const playerSelection = userInput;
+const playerSelection = userInput();
 const computerSelection = computerPlay();
+const playerScore = 0;
+const computerScore =0;
+
+function displayResults(playerScore, computerScore){
+    if (playerScore === 5){
+        alert('you win!');
+    } else if (computerScore === 5){
+        alert('computer wins!');
+    }
+
+}
+
 
 
     
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection,playerScore,computerScore) {
         playerSelection = playerSelection.toLowerCase();
         if (playerSelection === computerSelection){
             alert('its a tie!');
         }   else if (playerSelection === "rock" && computerSelection === "paper"){
             alert('you lose!');
+            computerScore = computerScore++;
         }   else if (playerSelection === "paper" && computerSelection === "scissors"){
             alert('you lose!');
+            computerScore = computerScore++;
         }   else if (playerSelection === "scissors" && computerSelection === "rock"){
             alert('you lose!');
+            computerScore = computerScore++;
         }   else {
             alert('you win!');
+            playerScore = playerScore++;
         }
+    return playerScore && computerScore;
     
-}  
 
-playRound(playerSelection, computerSelection);
+    
+}
+
+playRound(playerSelection, computerSelection, playerScore, computerScore);
